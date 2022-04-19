83559b5544cbcca1a6d2f1fad9cf8eb6fee727d7
Piotr <piotr.tchernobilski@gmail.com>
2022-04-19 11:09:14 +0200

    Ajout de la page ww-front/hotels, des données de tests pour affichage

--- src/Controller/HotelsController.php
+++ src/Controller/HotelsController.php
@@ -3,17 +3,18 @@
 namespace App\Controller;
 
 use App\Repository\HotelRepository;
+use App\Repository\SuitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class HotelsController extends AbstractController
 {
-    #[Route('/index', name: 'app_home')]
+    #[Route('/hotels', name: 'app_hotels')]
     public function index(HotelRepository $hotelRepository): Response
     {
-        return $this->render('www-front/home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('www-front/hotels/hotels.html.twig', [
+            'hotels' => $hotelRepository->viewHotels(),
         ]);
     }
 }
